13fe9eaa3ede4db63094b3667723a4805a1792b2
pc <pollobkumar1491@gmail.com>
2025-04-12 02:23:17 +0600

    2nd commit

---
+++ javaSrcipt.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const newBookBtn = document.getElementById("newBookBtn");
+  
+    newBookBtn.addEventListener("click", function () {
+      window.location.href = "new-book.html";
+    });
+  });
+  
